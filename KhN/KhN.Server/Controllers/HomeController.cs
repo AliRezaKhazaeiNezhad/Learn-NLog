a793b8c049e7f66568d3135ad97acd6c7588ff69
AliReza <khazaeinezhadalireza@gmail.com>
2022-03-27 14:43:19 +0430

    بروزرسانی

--- KhN/KhN.Server/Controllers/HomeController.cs
+++ KhN/KhN.Server/Controllers/HomeController.cs
@@ -17,13 +17,20 @@ namespace KhN.Server.Controllers
         [HttpGet]
         public string Get()
         {
-            _logger.LogTrace("Hello, this is the Trace!");
-            _logger.LogDebug("Hello, this is the Debug!");
-            _logger.LogWarning("Hello, this is the Warning!");
-            _logger.LogInformation("Hello, this is the Information!");
-            _logger.LogError("Hello, this is the Erorr!");
-            _logger.LogCritical("Hello, this is the Fatal(Critical)!");
+            #region Session21
+            //_logger.LogTrace("Hello, this is the Trace!");
+            //_logger.LogDebug("Hello, this is the Debug!");
+            //_logger.LogWarning("Hello, this is the Warning!");
+            //_logger.LogInformation("Hello, this is the Information!");
+            //_logger.LogError("Hello, this is the Erorr!");
+            //_logger.LogCritical("Hello, this is the Fatal(Critical)!");
+            #endregion
 
+
+            #region Session22
+            _logger.LogCritical("Hello, this is the Fatal(Critical)!");
+            _logger.LogCritical("Hello, this is the Fatal");
+            #endregion
 
             return "Hello World";
         }
